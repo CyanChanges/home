939a67244551bb1444f1f4049e9e5c40187daaf3
cyan <contact@cyans.me>
2024-08-31 22:27:05 +0800

    chore: defaults to netlify feat: automatic prefetch

--- app.vue
+++ app.vue
@@ -12,7 +12,7 @@ let items: MenuItem[] = [
     label: "Home",
     icon: PrimeIcons.HOME,
     route: "/",
-    disabled: (...args) => {
+    disabled: () => {
       return useTrimEnd(router.currentRoute.value.path, "/") == ""
     },
   },
@@ -20,7 +20,7 @@ let items: MenuItem[] = [
     label: "Blogs",
     icon: PrimeIcons.LIST,
     route: "/blog",
-    disabled: (...args) => {
+    disabled: () => {
       return useTrimEnd(router.currentRoute.value.path, "/") == "/blog"
     },
   }
@@ -30,8 +30,9 @@ let items: MenuItem[] = [
 <template>
   <Menubar :model="items" class="menubar">
     <template #item="{ item, props, hasSubmenu, root }">
-      <router-link class="flex items-center" v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-        <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+      <NuxtLink v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+        <div class="flex items-center">
+          <a :href="href" v-bind="props.action" @click="navigate">
           <span :class="item.icon"/>
           <span class="ml-2">{{ item.label }}</span>
           <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge"/>
@@ -42,7 +43,8 @@ let items: MenuItem[] = [
           <i v-if="hasSubmenu"
              :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
         </a>
-      </router-link>
+        </div>
+      </NuxtLink>
       <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
         <span :class="item.icon"/>
         <span class="ml-2">{{ item.label }}</span>
@@ -59,7 +61,7 @@ let items: MenuItem[] = [
       <div class="flex items-center gap-2">
         <IconField>
           <InputIcon class="pi pi-search"/>
-          <InputText disabled placeholder="Search" type="text" class="w-32 sm:w-auto"/>
+          <InputText disabled placeholder="Search" type="text"/>
         </IconField>
       </div>
     </template>
