abdbe164574ff4656ed61615935af4e407aa4566
cyan <contact@cyans.me>
2024-09-01 18:30:53 +0800

    chore: initial

---
+++ README.md
@@ -0,0 +1,41 @@
+# Cyan Home
+Built with Bun + Nuxt + PrimeVue
+
+
+## Browse 
+
+Try access any of following:
+
+> [!NOTE]
+> HTTPS for GitHub Pages didn't work for unknown reason.
+>
+
+| Defaults | Cloudflare(Netlify) | Netlify | GitHubPages |  
+| -------- | ------------------- | ------- | ----------- |
+| [cyans.me](https://cyans.me) | [www](https://www.cyans.me/) | [netlify.home](https://netlify.home.cyans.me/) | [ghpages.home](http://ghpages.home.cyans.me/) |
+
+## Build
+### Clone the repository
+```shell
+git clone https://github.com/CyanChanges/home.git cyans-home
+cd cyans-home
+```
+
+### Install
+We use Bun for both JavaScript Runtime and Package Runtime.
+
+```shell
+bun install
+```
+
+### Run 
+
+```shell
+bun run dev -o
+```
+
+### Build
+We use Bun for both JavaScript Runtime and Package Runtime.
+```shell
+bun run build
+```
---
+++ components/AppFooter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Fluid from 'primevue/fluid';
+</script>
+
+<template>
+  <span class="flex w-full m-2rem mt-5rem">
+    <footer class="footer m-auto text-center w-a h-a font-700">
+      <span class="t-cyan">CyanChanges</span> &copy; 2024. All rights reserved.
+      <br><br>
+      <span class="">
+        <a v-tooltip="'CyanChanges'" href="https://github.com/CyanChanges">GitHub</a> |
+        <a v-tooltip="'@cyanc:matrix.org'" href="https://matrix.to/@cyanc:matrix.org#/@cyanc:matrix.org">Matrix</a> |
+        <a v-tooltip="'cyanchanges'" type="text" href="https://discord.gg/hp86wkWf">Discord</a> |
+        <a v-tooltip="'CyanChanges@mastodon.social'" href="https://mastodon.social/@CyanChanges">Mastodon</a> |
+        <a v-tooltip="'ID: 298526251'" href="https://www.hoyolab.com/accountCenter/postList?id=298526251" class="color-dark-3"><del>HoYoLab</del></a>
+      </span>
+    </footer>
+  </span>
+</template>
+
+<style scoped>
+.t-cyan {
+  background: linear-gradient(25deg, #2dceff, #2dffd8);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight: 1000;
+}
+</style>
