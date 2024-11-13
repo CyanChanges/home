85fdb6da6c90eb7c9c07fed3b67dda9609950dbf
cyan <contact@cyans.me>
2024-11-14 01:56:05 +0800

    docs: add

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,9 @@ const greeting = toRaw(data.value)
   <Card>
     <template #content>
       {{ greeting }}
-      <h3>LSPosed Internal Test</h3>
-      <p>极其先进</p>
-      <p>遥遥领先</p>
-      <p>加密混淆</p>
-      <p>完全闭源</p>
-      <p>禁止分享</p>
-      <p>泄漏溯源</p>
+      <divider/>
+      {{ t("new-project-notice") }}
+      <NuxtLink href="/blog/new-project-in-2024">{{ t("new-project-title")}}</NuxtLink>
     </template>
   </Card>
 </template>
@@ -29,10 +25,16 @@ const greeting = toRaw(data.value)
 <i18n lang="yaml">
 en:
   hello: Hello
+  new-project-notice: Checkout the preview of my new project
+  new-project-title: Blog Post
 ja:
   hello: こんにちは、
 zhHans:
   hello: 你好
+  new-project-notice: "新项目预告:"
+  new-project-title: Blog Post
 zh-Hant:
   hello: 你好
+  new-project-notice: "新項目預告:"
+  new-project-title: Blog Post
 </i18n>
