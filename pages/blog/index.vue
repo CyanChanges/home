79cacf1b1b3eb201f67f12dd3eeff83530fa8bb2
CyanChanges <CyanChanges@users.noreply.github.com>
2024-12-22 04:36:00 +0000

    deploy 3ce9fccc805dcc117b423341a36082bac2e9a30c

---
+++ pages/blog/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// const route = useRoute()
+useSeoMeta({
+  robots: 'index, follow'
+})
+</script>
+
+<template>
+  <ContentList path="/blog" v-slot="{ list }">
+    <div v-for="article in list.filter(v=>!v.hidden)" :key="article._path">
+      <router-link :to="article?._path?.toString() ?? '/blog'">
+        <Card v-ripple>
+          <template #title>{{ article.title }}</template>
+          <template #subtitle>{{ article.description }}</template>
+        </Card>
+      </router-link>
+    </div>
+  </ContentList>
+</template>
